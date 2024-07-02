fabf39b2f64cc1c41464fb3af836a0abc7af6276
arch24 <wyz2016zxc@outlook.com>
2024-07-02 19:36:33 +0800

    debug

--- config.h
+++ config.h
@@ -1,11 +1,13 @@
 #define CMDLENGTH 100
-#define DELIMITER "  "
-#define CLICKABLE_BLOCKS
+#define DELIMITER " "
+#define CLICKABLE_BLOCKS 1
 
 
 const Block blocks[] = {
-	BLOCK("block_resources",	5, 0),
+	BLOCK("block_time",	60, 2),
 	BLOCK("block_battery", 1, 1),
-	BLOCK("block_date",	60, 2),
+	BLOCK("block_light",	1, 3),
+	BLOCK("block_resources",	1, 5),
+	BLOCK("block_pulse",	1, 4),
 };
 
