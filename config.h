8f3d7e6309670386fa14ea28dd0360737f598a8e
arch24 <wyz2016zxc@outlook.com>
2024-07-05 16:10:56 +0800

    update

--- config.h
+++ config.h
@@ -6,8 +6,8 @@
 const Block blocks[] = {
 	BLOCK("block_time",	60, 2),
 	BLOCK("block_battery", 1, 1),
-	BLOCK("block_light",	1, 3),
+	BLOCK("block_light",	0, 3),
 	BLOCK("block_resources",	1, 5),
-	BLOCK("block_pulse",	1, 4),
+	BLOCK("block_pulse",	0, 4),
 };
 
