9e7ec39279f1d363cb1c30834496de9ab5f5e911
arch24 <wyz2016zxc@outlook.com>
2024-07-12 12:10:43 +0800

    update

--- config.h
+++ config.h
@@ -4,10 +4,11 @@
 
 
 const Block blocks[] = {
-	BLOCK("block_time",	60, 2),
-	BLOCK("block_battery", 1, 1),
-	BLOCK("block_light",	0, 3),
-	BLOCK("block_resources",	1, 5),
-	BLOCK("block_pulse",	0, 4),
+	// BLOCK("block_resources",1 ,1),
+	// BLOCK("block_light"    ,0 ,1),
+	BLOCK("block_wifi"     ,5 ,1),
+	BLOCK("block_pulse"    ,0 ,2),
+	BLOCK("block_battery"  ,60,3),
+	BLOCK("block_time"     ,60,4),
 };
 
