e05d6bb8bba1c0678dbd5411dd0574677a9d27ec
arch24 <wyz2016zxc@outlook.com>
2024-07-20 18:45:39 +0800

    move blocks

--- config.h
+++ config.h
@@ -5,10 +5,14 @@
 
 const Block blocks[] = {
 	// BLOCK("block_resources",1 ,1),
+  // bottombar
 	BLOCK("block_wifi"     ,1 ,1),
-	BLOCK("block_light"    ,0 ,2),
 	BLOCK("block_pulse"    ,0 ,3),
 	BLOCK("block_battery"  ,60,4),
+  // split
+  {"echo ';'" ,0,0},
+  // topbar 
+	BLOCK("block_light"    ,0 ,2),
 	BLOCK("block_time"     ,60,5),
 };
 
